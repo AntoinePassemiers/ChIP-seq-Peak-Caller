63a2dd35db2e6342a4083c91f3ab427c5af5a9af
AntoinePassemiers <apassemi@ulb.ac.be>
2019-05-12 19:13:09 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# setup.py
+# author : Antoine Passemiers
+
+from setuptools import setup
+
+
+setup(
+    name='chipseq',
+    version='1.0.0',
+    description='ChIP-seq peak caller',
+    url='https://github.com/AntoinePassemiers/ChIP-seq-Peak-Caller',
+    author='Antoine Passemiers',
+    author_email='apassemi@ulb.ac.be',
+    packages=['chipseq'])
